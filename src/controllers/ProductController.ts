53ef920962a1085d32b8a6b7b3758175dbb75db2
ricardoalves-dev <ricardo-j-alves@hotmail.com>
2024-01-13 00:10:55 -0300

    Modify the Product Controller constructor

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from "express";
+import mongoose from "mongoose";
+import { Model } from "../model/Model";
+import { IProduct } from "../schemas/ProductSchema";
 
 export class ProductController{
-  public findAll(req: Request, res: Response): void{
-    res.status(200).send('Routing Test');
+  private productModel: Model<IProduct>;
+
+  constructor(productModel: Model<IProduct>){
+    this.productModel = productModel;
+  }
+
+  public async find(req: Request, res: Response): Promise<void>{
+    res.status(200).json(await this.productModel.getModel().find());
+  }
+
+  public async save(req: Request, res: Response): Promise<void>{
+    try { 
+      const product = this.productModel.getModel().create(req.body);    
+      res.status(201).json(product);
+    } catch (error) {
+      res.status(400).send((`Error: ${error instanceof Error ? error.message : String(error)}`));
+    }    
   }
 }
