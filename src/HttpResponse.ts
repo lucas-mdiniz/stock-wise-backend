e341aa21db24411a9117268ff85afb5aff2677e5
ricardoalves-dev <ricardo-j-alves@hotmail.com>
2024-01-15 08:41:45 -0300

    Create the HttpResponse class

---
+++ src/HttpResponse.ts
@@ -0,0 +1,42 @@
+export class HttpResponse{
+  private _data!: object | null;
+  private _error: string;
+  private _count: number;
+
+  constructor(){
+    this._error = '';
+    this._count = 0;
+  }
+
+  get data(): object | null{
+    return this._data;
+  }
+
+  set data(data: object | null) {
+    this._data = data;
+
+    if (data){
+      this._count = Array.isArray(data) ? data.length : 1;
+    }    
+  }
+
+  get error(): string{
+    return this._error;
+  }
+
+  set error(error: string){
+    this._error = error;
+  }
+
+  get count(): number{
+    return this._count;
+  }
+
+  toJson(){
+    return {
+      "data": this._data ? this._data : {},
+      "error": this._error,
+      "count": this._count
+    }
+  }
+}
