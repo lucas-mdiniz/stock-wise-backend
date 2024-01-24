c61aca4b17d644cb6234c27dbb2b14fd86c47446
ricardoalves-dev <ricardo-j-alves@hotmail.com>
2024-01-24 07:54:45 -0300

    Modify the ProductController structure

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -1,77 +1,56 @@
-import { Request, Response } from "express";
-import { Model } from "../model/Model";
-import { IProduct } from "../schemas/ProductSchema";
-import { HttpResponse } from "../HttpResponse";
+import { Request, Response } from 'express';
+import { Model } from '../model/Model';
+import { IProduct } from '../schemas/ProductSchema';
+import { HttpResponse } from '../HttpResponse';
+import { NotFoundError } from '../errors/NotFoundError';
+import status from 'http-status';
 
-export class ProductController{
+export class ProductController {
   private productModel: Model<IProduct>;
 
-  constructor(productModel: Model<IProduct>){
+  constructor(productModel: Model<IProduct>) {
     this.productModel = productModel;
   }
 
-  public async find(req: Request, res: Response): Promise<void>{
-    const httpResponse: HttpResponse = new HttpResponse();
-
-    try {
-      httpResponse.data = await this.productModel.getModel().find();  
-      res.status(200);
-    } catch (error) {
-      httpResponse.error = (`Error: ${error instanceof Error ? error.message : String(error)}`);
-      res.status(400);      
-    }    
-    res.json(httpResponse.toJson());    
+  public async find(req: Request, res: HttpResponse): Promise<void> {
+    res.data = await this.productModel.getModel().find();
+    res.status = status.OK;
   }
 
-  public async findById(req: Request, res: Response): Promise<void>{
-    const httpResponse: HttpResponse = new HttpResponse();
+  public async findById(req: Request, res: HttpResponse): Promise<void> {
+    res.data = await this.productModel.getModel().findById(req.params.id);
 
-    try {
-      httpResponse.data = await this.productModel.getModel().findById(req.params.id);  
-      res.status(200);
-    } catch (error) {
-      httpResponse.error = (`Error: ${error instanceof Error ? error.message : String(error)}`);
-      res.status(400);      
-    }    
-    res.json(httpResponse.toJson());
+    if (!res.data) {
+      throw new NotFoundError(`Produto de id ${req.params.id} não encontrado`);
+    }
+
+    res.status = status.OK;
   }
 
-  public async save(req: Request, res: Response): Promise<void>{
-    const httpResponse: HttpResponse = new HttpResponse();
-
-    try { 
-      httpResponse.data = await this.productModel.getModel().create(req.body);    
-      res.status(201);
-    } catch (error) {
-      httpResponse.error = (`Error: ${error instanceof Error ? error.message : String(error)}`);
-      res.status(400);
-    }    
-    res.json(httpResponse.toJson());
+  public async save(req: Request, res: HttpResponse): Promise<void> {
+    res.data = await this.productModel.getModel().create(req.body);
+    res.status = status.CREATED;
   }
 
-  public async update(req: Request, res: Response): Promise<void>{
-    const httpResponse: HttpResponse = new HttpResponse();
+  public async update(req: Request, res: HttpResponse): Promise<void> {
+    res.data = await this.productModel.getModel().findByIdAndUpdate(req.params.id, req.body, { new: true });
 
-    try { 
-      httpResponse.data = await this.productModel.getModel().findByIdAndUpdate(req.params.id, req.body, {new: true});    
-      res.status(200);
-    } catch (error) {
-      httpResponse.error = (`Error: ${error instanceof Error ? error.message : String(error)}`);
-      res.status(400);
-    }    
-    res.json(httpResponse.toJson());
+    if (!res.data) {
+      throw new NotFoundError(`Produto de id ${req.params.id} não encontrado`);
+    }
+
+    res.status = status.OK;
   }
 
-  public async delete(req: Request, res: Response): Promise<void>{
-    const httpResponse: HttpResponse = new HttpResponse();
+  public async delete(req: Request, res: HttpResponse): Promise<void> {
+    const data = await this.productModel
+      .getModel()
+      .findByIdAndDelete(req.params.id);
 
-    try { 
-      await this.productModel.getModel().findByIdAndDelete(req.params.id);    
-      res.status(200);
-    } catch (error) {
-      httpResponse.error = (`Error: ${error instanceof Error ? error.message : String(error)}`);
-      res.status(400);
-    }    
-    res.json(httpResponse.toJson());
+    if (!data) {
+      throw new NotFoundError(`Produto de id ${req.params.id} não encontrado`);
+    }
+
+    res.status = status.NO_CONTENT;
   }
 }
